db5724f3f09a88a5454fc6b8f5b6d335a589bcd7
Eduardo Ramos <eduardorxs@gmail.com>
2022-01-24 15:36:29 -0300

    serviços para listar produtos e listar apenas um produto

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,16 @@
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const products = productsRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/errors/AppError';
+import { ProductRepository } from './../typeorm/repositories/ProductsRepository';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Produto não encontrado');
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
