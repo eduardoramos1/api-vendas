df0c0e22302a72706f55ecd0bc7b6fc57197a51e
Eduardo Ramos <eduardorxs@gmail.com>
2022-01-26 16:00:31 -0300

    Controller para Users

---
+++ src/modules/Users/controllers/UsersController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../typeorm/services/CreateUserService';
+import ListUserService from '../typeorm/services/ListUserService';
+export default class UsersController {
+  public async listUsers(req: Request, res: Response): Promise<Response> {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return res.json(users);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    return res.json(user);
+  }
+}
