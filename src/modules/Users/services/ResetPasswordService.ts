d1ce1f5a60a1519abb6a0f93d94af670d7c08ae6
Eduardo Ramos <eduardorxs@gmail.com>
2022-01-31 15:51:15 -0300

    Serviço para reset de senha

---
+++ src/modules/Users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import { hash } from 'bcryptjs';
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isAfter, addHours } from 'date-fns';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('Token não existe');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+    if (!user) {
+      throw new AppError('Usuário não existe');
+    }
+
+    // verificando se token já expirou
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    // verifica se a hora atual está a frente do tempo estipulado para o token ser valido
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expirado');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
