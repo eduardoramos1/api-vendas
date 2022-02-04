744aad856899f7854961e963e668dedbe298e385
Eduardo Ramos <eduardorxs@gmail.com>
2022-02-04 15:09:43 -0300

    Handlebars e HandlebarsMailTemplate

---
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -0,0 +1,22 @@
+import Handlebars from 'handlebars';
+
+// Porque não sabemos exatamente quais serão as informações que estarão dentro de variables é possivel deixar dinamico dessa forma
+interface ITemplateVariables {
+  [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariables;
+}
+
+export default class HandlebarsMailTemplate {
+  public async parse({
+    template,
+    variables,
+  }: IParseMailTemplate): Promise<string> {
+    const parseTemplate = Handlebars.compile(template);
+
+    return parseTemplate(variables);
+  }
+}
